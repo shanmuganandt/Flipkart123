d9d51106acc39c2ea36a6fb3c8ead73aac92e4de
Anand12345 <shanmuganand29@gmail.com>
2022-07-26 15:10:16 +0530

    code added

--- src/org/maths/Addtion.java
+++ src/org/maths/Addtion.java
@@ -3,14 +3,12 @@ package org.maths;
 public  class Addtion {
 	
 	
-		
-	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		
 		int a=10,b=20, c;
 		c=a+b;
 		System.out.println(c);
-		
+		System.out.println("hiai");
 	}
 
 }
